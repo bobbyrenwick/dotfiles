eaa801f0b7aa74d6366e208726f013421bcf9449
Bob Renwick <bob.renwick@gmail.com>
2016-09-28 07:26:57 +0100

    add brew cask installs for most things

---
+++ install.sh
@@ -0,0 +1,8 @@
+brew cask install shiftit
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install slack
+brew cask install iterm2
+
